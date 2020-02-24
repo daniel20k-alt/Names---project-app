11779ea9760406a03162099ec29320def62e19fa
Testing_author <testing_beta@mail.com>
2020-02-24 23:06:12 +0200

    Configured the imageView and Label outlets.

---
+++ Names - project app/PersonCell.swift
@@ -0,0 +1,19 @@
+//
+//  PersonCell.swift
+//  Names - project app
+//
+//  Created by DDDD on 24/02/2020.
+//  Copyright © 2020 MeerkatWorks. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+
+    @IBOutlet var name: UILabel!
+    
+
+
+}
