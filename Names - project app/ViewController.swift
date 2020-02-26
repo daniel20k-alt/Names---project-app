0a4f1716769ed483622fc25174f2f29694a682a0
Testing_author <testing_beta@mail.com>
2020-02-26 07:35:45 +0200

    Creating the path to the image directory.

--- Names - project app/ViewController.swift
+++ Names - project app/ViewController.swift
@@ -40,7 +40,13 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         guard let image = info[.editedImage] as? UIImage else { return }
         
         let imageName = UUID().uuidString
-        let imagePath = getDocumentsDirectory().appendingpPathComponent(imageName)
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        dismiss(animated: true)
     }
     
     func getDocumentsDirectory() -> URL {
