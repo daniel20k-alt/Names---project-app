48a399d387f8def9fc38776391280372a89d344b
Testing_author <testing_beta@mail.com>
2020-02-28 08:28:07 +0200

    Assign a custom name to our image, allowing users to rename their image.

--- Names - project app/ViewController.swift
+++ Names - project app/ViewController.swift
@@ -67,9 +67,6 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         people.append(person)
         collectionView.reloadData()
         
-        
-        
-        
         dismiss(animated: true)
     }
     
@@ -78,7 +75,19 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         return paths[0]
     }
     
-    
-    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.item]
+        
+        let ac = UIAlertController(title: "Rename Person", message: nil, preferredStyle: .alert)
+        
+        ac.addTextField()
+        
+        ac.addAction(UIAlertAction(title: "Ok", style: .default) { [weak self, weak ac] _ in
+        guard let newName = ac?.textFields?[0].text else { return }
+        person.name = newName
+            self?.collectionView.reloadData()
+    })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
 }
-
+}
